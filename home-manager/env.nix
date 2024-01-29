79f55ac84139845b05820285f8eb1bc4d6640a6a
David West <1122691+dbwest@users.noreply.github.com>
2024-01-29 00:08:12 -0500

    temp setting

--- home-manager/env.nix
+++ home-manager/env.nix
@@ -3,5 +3,7 @@
 }:
 
 {
+  defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
+  defaults delete -g ApplePressAndHoldEnabled
   PATH = "/etc/profiles/per-user/${username}/bin:$PATH";
 }
